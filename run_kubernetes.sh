0d79f64389dfb2f035036ecf5abf2ccb7ed1cb94
irschad <irniz@rediffmail.com>
2020-10-24 13:43:47 +0000

    Added steps to deploy with Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=irschad/mlpredict
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlpredict --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlpredict 8080:80
