9b61d34f447c98d25f67404cc64327fecd3be624
irschad <irniz@rediffmail.com>
2020-10-24 13:39:25 +0000

    Added dockerpath, and authenticate, tag and push commands

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=irschad/mlpredict
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username irschad
+docker tag mlpredict $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
+
